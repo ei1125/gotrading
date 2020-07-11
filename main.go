5c1036957aa2896200c4db8b8f981db249447f31
ei1125 <i.dont.like.monday1125@gmail.com>
2020-07-11 23:28:33 +0900

    buy-and-sell

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"./bitflyer"
 	"./config"
@@ -13,14 +12,25 @@ func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 
-	tickerChannel := make(chan bitflyer.Ticker)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	for ticker := range tickerChannel {
-		fmt.Println(ticker)
-		fmt.Println(ticker.GetMidPrice())
-		fmt.Println(ticker.DateTime())
-		fmt.Println(ticker.TruncateDateTime(time.Second))
-		fmt.Println(ticker.TruncateDateTime(time.Minute))
-		fmt.Println(ticker.TruncateDateTime(time.Hour))
+	order := &bitflyer.Order{
+		ProductCode:     config.Config.ProdectCode,
+		ChildOrderType:  "MARKET",
+		Side:            "BUY",
+		Size:            0.01,
+		MinuteToExpires: 1,
+		TimeInForce:     "GTC",
 	}
+	res, _ := apiClient.SendOrder(order)
+	fmt.Println(res.ChildOrderAcceptanceID)
+
+	// tickerChannel := make(chan bitflyer.Ticker)
+	// go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+	// for ticker := range tickerChannel {
+	// 	fmt.Println(ticker)
+	// 	fmt.Println(ticker.GetMidPrice())
+	// 	fmt.Println(ticker.DateTime())
+	// 	fmt.Println(ticker.TruncateDateTime(time.Second))
+	// 	fmt.Println(ticker.TruncateDateTime(time.Minute))
+	// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
+	// }
 }
